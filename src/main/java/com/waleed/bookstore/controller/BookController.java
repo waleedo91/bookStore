e190803288bb1f1c9334da14c1fead47520b4c21
Waleed <wsaleh1991@gmail.com>
2022-11-29 23:10:47 -0600

    initial commit for book store api

---
+++ src/main/java/com/waleed/bookstore/controller/BookController.java
@@ -0,0 +1,29 @@
+package com.waleed.bookstore.controller;
+
+import com.waleed.bookstore.dto.BookDto;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.print.Book;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/books")
+public class BookController {
+
+    @GetMapping
+    public ResponseEntity<List<BookDto>> getBooks() {
+        BookDto book = BookDto.builder()
+                .title("My First Book")
+                .build();
+        List<BookDto> books = new ArrayList<>();
+        books.add(book);
+        return ResponseEntity.ok(books);
+    }
+
+
+}
